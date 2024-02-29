eebfc728a50730cc462551507e125c20e6220283
juannpmari <juannpmari@gmail.com>
2024-02-28 21:57:23 -0300

    fixed bug with config file loading

--- training_pipeline/pipeline.py
+++ training_pipeline/pipeline.py
@@ -4,6 +4,7 @@ from training import Trainer
 
 if __name__=="__main__":
     config_path = 'config.json'
-    config = json.load(config_path)
+    with open(config_path, 'r') as file:
+        config = json.load(file)
     current_time = Preprocessor(config)()
     Trainer(current_time)()
